01affcccee7999fc7a93d2d738642ee78de55dd7
don <chineseDon@163.com>
2019-03-18 21:36:55 +0800

    router resolve

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var router = require('./router');
-
+/* 路由 */
 app.use(router);
 
 app.listen('3000', () => {
--- router.js
+++ router.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function timeLog(req, res, next) => {
+	console.log("time is :" + Date.now());
+	next();
+})
+
 router.get('/', (req, res) => {
 	console.log('连接已经建立')
 	res.send('hello bird')
