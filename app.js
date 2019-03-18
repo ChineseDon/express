e11bab001f10e956c3264241f9b4b5d256daab57
don <chineseDon@163.com>
2019-03-18 21:28:29 +0800

    router set

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
 var express = require('express');
-
 var app = express();
+var router = require('./router');
 
-app.get('/', (req, res) => {
-	res.send('hello express');
-})
-
-app.all('/secret', (req, res, next) => {
-	console.log('secret 连接已建立...');
-	next();
-	res.send('secret 连接建立成功...')
-})
-
-app.get('/post', (req, res) => {
-	console.log(req.url);
-	console.log(req.params)
-	console.log(req.query);
-	res.send('接受参数成功');
-})
+app.use(router);
 
 app.listen('3000', () => {
 	console.log('连接已建立');
