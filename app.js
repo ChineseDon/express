d5237f868e74f4e65f99366b4a7c69367af4fa83
don <chineseDon@163.com>
2019-03-18 22:42:54 +0800

    express static file read

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 var router = require('./router');
+
+// 配置art-template
+app.engine('html', require('express-art-template'));
+
+// 静态资源设置
+app.use('/public/', express.static('./public/'));
+
 /* 路由 */
 app.use(router);
 
--- router.js
+++ router.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(function timeLog(req, res, next) => {
+var liuyan = [
+	{
+		id: 1,
+		name: '张三',
+		content: '1231231'
+	},
+	{
+		id: 2,
+		name: '张三a',
+		content: '1231231asdfa'
+	},
+	{
+		id: 3,
+		name: '张三b',
+		content: '1231231dsdd'
+	},
+	{
+		id: 4,
+		name: '张三c',
+		content: '1231231asdfa'
+	}
+]
+
+router.use(function timeLog(req, res, next) {
 	console.log("time is :" + Date.now());
 	next();
 })
 
 router.get('/', (req, res) => {
 	console.log('连接已经建立')
-	res.send('hello bird')
+	res.render('index.html', {
+		liuyan: liuyan
+	})
+})
+
+router.get('/add', (req, res) => {
+	res.render('add.html');
 })
 
 module.exports = router;
