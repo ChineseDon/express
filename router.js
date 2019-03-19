8130f50ea472bec9f228c86031d88564c519c1b6
don <1776565321@qq.com>
2019-03-19 17:04:22 +0800

    express 留言板

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+// body-parser配置
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var liuyan = [
 	{
@@ -38,6 +43,51 @@ router.get('/', (req, res) => {
 
 router.get('/add', (req, res) => {
 	res.render('add.html');
+})
+
+router.get('/addLiuyan', (req, res) => {
+	console.log(req.query);
+	liuyan.push({
+		id: liuyan.length + 1,
+		name: req.query.name,
+		content: req.query.liuyan
+	})
+	res.redirect('/')
+})
+
+router.get('/delete/:id', (req, res) => {
+	console.log(req.params.id)
+	if(req.params.id) {
+		liuyan.splice(req.params.id-1, 1)
+		console.log(liuyan)
+		res.redirect('/');
+	} else {
+		res.end('404')
+	}
+})
+
+router.get('/edit/:id', (req, res) => {
+	console.log(req.params.id)
+	var liuyanUser = liuyan.filter((item, index) => {
+		console.log(item)
+		return item.id == req.params.id;
+	})
+	console.log(liuyanUser)
+	res.render('edit.html', {
+		liuyan: liuyanUser[0]
+	})
+})
+
+router.post('/post', urlencodedParser, (req, res) => {
+	console.log(req.body);
+	var editContent = req.body;
+	var index = liuyan.findIndex((item, index) => {
+		return req.body.id == item.id;
+	})
+	console.log(index);
+	liuyan.splice(index, 1, editContent);
+	console.log(liuyan);
+	res.redirect('/')
 })
 
 module.exports = router;
